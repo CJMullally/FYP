c09145136de120e9ec89b251512c9a1db6b78d75
CianMullally <cianmullally@msn.com>
2017-10-22 22:43:33 +0100

    Added sorting and searching functionality to product page

--- FYPInitial/FYPInitial/Controllers/ProductController.cs
+++ FYPInitial/FYPInitial/Controllers/ProductController.cs
@@ -6,22 +6,50 @@ using System.Data;
 using System.Web.Mvc;
 using FYPInitial.Models;
 
+// This controller manages the product CRUD functionality 
 namespace FYPInitial.Controllers
 {
     public class ProductController : Controller
     {
         // GET: Product
-        public ActionResult Index()
+        // Populates the list on the index page
+       
+        public ActionResult Index(string sortOrder, string searchString)
         {
+            ViewBag.ProductNameSortParm = String.IsNullOrEmpty(sortOrder) ? "product_desc" : "";
+            ViewBag.PriceSortParm = sortOrder == "Price" ? "price_desc" : "Price";
             List<product> productList = new List<product>();
             using (DBModels dbModel = new DBModels())
             {
                 productList = dbModel.products.ToList<product>();
+                var products = from s in dbModel.products
+                               select s;
+
+                if (!String.IsNullOrEmpty(searchString))
+                {
+                    products = products.Where(s => s.ProductName.Contains(searchString));
+                }
+
+                switch (sortOrder)
+                {
+                    case "product_desc":
+                        products = products.OrderByDescending(s => s.ProductName);
+                        break;
+                    case "price_desc":
+                        products = products.OrderByDescending(s => s.Price);
+                        break;
+                    default:
+                        products = products.OrderBy(s => s.ProductName);
+                        break;
+                }
+            return View(products.ToList());
+           
             }
-                return View(productList);
+            
         }
 
         // GET: Product/Details/5
+        // Returns a view containing product details
         public ActionResult Details(int id)
         {
             product productModel = new product();
@@ -33,12 +61,14 @@ namespace FYPInitial.Controllers
         }
 
         // GET: Product/Create
+        // Returns a view where a new product can be added
         public ActionResult Create()
         {
             return View(new product());
         }
 
         // POST: Product/Create
+        // Takes the details from the product create view and posts them to the server
         [HttpPost]
         public ActionResult Create(product productModel)
         {
@@ -51,6 +81,7 @@ namespace FYPInitial.Controllers
         }
 
         // GET: Product/Edit/5
+        // Returns a view where product details can be edited
         public ActionResult Edit(int id)
         {
             product productModel = new product();
@@ -62,6 +93,7 @@ namespace FYPInitial.Controllers
         }
 
         // POST: Product/Edit/5
+        // Takes the new details from the product edit view and posts them to the server
         [HttpPost]
         public ActionResult Edit(product productModel)
         {
@@ -74,6 +106,7 @@ namespace FYPInitial.Controllers
          }
 
         // GET: Product/Delete/5
+        // Returns a view confirming product deletion
         public ActionResult Delete(int id)
         {
             product productModel = new product();
@@ -85,6 +118,7 @@ namespace FYPInitial.Controllers
         }
 
         // POST: Product/Delete/5
+        // Posts confirmation of product deletion to the server and deletes the product
         [HttpPost]
         public ActionResult Delete(int id, FormCollection collection)
         {
